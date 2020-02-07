e236b073c3e6f0fa6bc9f8ca7e658b806eee53d7
vinod-nagaraj <vinodnagaraj043@gmail.com>
2020-02-07 20:02:54 +0530

    fourth commit

--- src/main/java/com/foodie/homeslice/service/ItemService.java
+++ src/main/java/com/foodie/homeslice/service/ItemService.java
@@ -1,9 +1,6 @@
 package com.foodie.homeslice.service;
 
-import java.util.List;
-
 import com.foodie.homeslice.dto.ItemsResponseDto;
-import com.foodie.homeslice.entity.Item;
 
 public interface ItemService {
 	ItemsResponseDto getAllItems();
